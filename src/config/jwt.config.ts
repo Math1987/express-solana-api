8fe11dc792b601da55a2087429e0a1cf8d00f324
math17 <mathieucolla@gmail.com>
2022-02-26 09:15:25 +0100

    feat: add token duration, 1 day by default

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -2,8 +2,10 @@ import jwt from "jsonwebtoken" ;
 const secret = "74c1ce27-c663-4e6e-b9a9-aa726652dcd2" ;
 
 
-export const createToken = ( datas : any ) => {
-    return jwt.sign(datas, secret);
+export const createToken = ( datas : any, secondsDuration = 60 * 60 * 24 ) => {
+    return jwt.sign(datas, secret, {
+        expiresIn : secondsDuration
+    });
 }
 export const readToken = ( token : string ) : Promise<any> => {
     return new Promise((resolve, reject) => {
